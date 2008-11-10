11791dab2c0392a17fd3ff3f3fa009ab54d594c0
Andreas J. Koenig <k@k75.(none)>
2008-11-10 23:19:54 +0100

    this will be 0.0.19

--- lib/CPAN/Testers/ParseReport.pm
+++ lib/CPAN/Testers/ParseReport.pm
@@ -28,7 +28,7 @@ CPAN::Testers::ParseReport - parse reports to www.cpantesters.org from various s
 
 =cut
 
-use version; our $VERSION = qv('0.0.18');
+use version; our $VERSION = qv('0.0.19');
 
 =head1 SYNOPSIS
 
